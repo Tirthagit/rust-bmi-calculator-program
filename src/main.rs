bce62feccf0b5179b682820e6f6f79fd4abe85da
Truetirtha <patitirtharaj@gmail.com>
2025-09-08 11:40:04 +0530

    Implement BMI calculator CLI with: - Weight and height input parsing - BMI computation and rounding - BMI category message output - Validation for realistic ranges

--- src/main.rs
+++ src/main.rs
@@ -13,26 +13,65 @@ fn main() {
         .read_line(&mut height_input)
         .expect("Please enter valid height");
 
-    let weight: f64 = weight_input
-        .trim()
-        .parse()
-        .expect("Please enter a valid weight");
-    let height: f64 = height_input
-        .trim()
-        .parse()
-        .expect("Please enter a valid weight");
+    let weight: f64 = weight_input_parsing(weight_input);
+    let height: f64 = height_input_parsing(height_input);
 
     bmi_calculator(weight, height);
 }
 
+// Weight bondary constants
+const MIN_WEIGHT: f64 = 0.0;
+const MAX_WEIGHT: f64 = 500.0;
+
+// Height boundary constants
+const MIN_HEIGHT: f64 = 0.5;
+const MAX_HEIGHT: f64 = 3.0;
+
 fn bmi_calculator(weight: f64, height: f64) {
-    if weight < 0.0 || weight > 500.0 {
+    if weight < MIN_WEIGHT || weight > MAX_WEIGHT {
         println!("Weight cannot be below 0 or above 500!")
     }
-    if height < 0.5 || height > 3.0 {
+    if height < MIN_HEIGHT || height > MAX_HEIGHT {
         println!("Height cannot be lower than 0.5 metres or higher than 3.0 metres!")
     }
     let mut bmi = weight / (height * height);
+    let message = bmi_message(bmi);
     bmi = (bmi * 100.0).round() / 100.0;
-    println!("{bmi}");
+    println!("Your B.M.I is: {bmi} --> {message}");
+}
+
+fn bmi_message(bmi: f64) -> &'static str {
+    if bmi < 16.0 {
+        "Severe Thinness"
+    } else if bmi >= 16.0 && bmi < 17.0 {
+        "Moderate Thinness"
+    } else if bmi >= 17.0 && bmi < 18.5 {
+        "Mild Thinness"
+    } else if bmi >= 18.5 && bmi < 25.0 {
+        "Normal"
+    } else if bmi >= 25.0 && bmi < 30.0 {
+        "Overweight!"
+    } else if bmi >= 30.0 && bmi < 35.0 {
+        "Obese Class I"
+    } else if bmi >= 35.0 && bmi < 40.0 {
+        "Obese Class II"
+    } else if bmi >= 40.0 {
+        "Obese Class III"
+    } else {
+        "Invalid"
+    }
+}
+
+fn weight_input_parsing(input: String) -> f64 {
+    return input
+        .trim()
+        .parse()
+        .expect("Please enter a valid a valid weight");
+}
+
+fn height_input_parsing(input: String) -> f64 {
+    return input
+        .trim()
+        .parse()
+        .expect("Please enter a valid a valid height");
 }
